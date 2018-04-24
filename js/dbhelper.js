87dfc4c7bf58b6d6c5da54639a75a6de2ecb3371
andrealeardini <andrea@wikilear.it>
2018-04-24 11:25:04 +0200

    feat: Update code to upload the restaurants from Sails Server.
    Add jpg to image path

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -20,8 +20,7 @@ class DBHelper {
         xhr.open('GET', DBHelper.DATABASE_URL);
         xhr.onload = () => {
             if (xhr.status === 200) { // Got a success response from server!
-                const json = JSON.parse(xhr.responseText);
-                const restaurants = json.restaurants;
+                const restaurants = JSON.parse(xhr.responseText);
                 callback(null, restaurants);
             } else { // Oops!. Got an error from server.
                 const error = (`Request failed. Returned status of ${xhr.status}`);
@@ -150,7 +149,7 @@ class DBHelper {
      * Restaurant image URL.
      */
     static imageUrlForRestaurant(restaurant) {
-        return (`/img/${restaurant.photograph}`);
+        return (`/img/${restaurant.photograph}.jpg`);
     }
 
     /**
