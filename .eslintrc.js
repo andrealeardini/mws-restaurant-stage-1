385074fc7d5abd4cbeeb9faf2b89b7d5bac1cc74
andrealeardini <andrea@wikilear.it>
2018-07-02 18:29:59 +0200

    chore: git syncro

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,22 @@
 module.exports = {
     "env": {
         "browser": true,
-<<<<<<< Updated upstream
         "es6": true,
         "node": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaVersion": 2015,
-=======
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
->>>>>>> Stashed changes
         "sourceType": "module"
     },
     "rules": {
         "indent": [
             "error",
-<<<<<<< Updated upstream
             2
-=======
-            4
->>>>>>> Stashed changes
         ],
         "linebreak-style": [
             "error",
             "windows"
-<<<<<<< Updated upstream
         ],
         "quotes": [
             "error",
@@ -39,8 +27,5 @@ module.exports = {
             "always"
         ],
         "no-console": "off"
-=======
-        ]
->>>>>>> Stashed changes
     }
 };
